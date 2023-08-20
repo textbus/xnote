6ba426829cf9f039a35b19a1bf3bc2b33a0e59da
tbhuabi <464328895@qq.com>
2023-08-20 17:57:45 +0800

    feat: 优化行内工具条交互

--- src/plugins/toolbar/toolbar.plugin.tsx
+++ src/plugins/toolbar/toolbar.plugin.tsx
@@ -1,7 +1,7 @@
 import { Plugin } from '@textbus/core'
 import { VIEW_DOCUMENT } from '@textbus/platform-browser'
-import { Application, Injector, viewfly } from '@viewfly/core'
-import { DomRenderer } from '@viewfly/platform-browser'
+import { Application, Injector } from '@viewfly/core'
+import { createApp } from '@viewfly/platform-browser'
 
 import { Toolbar } from './toolbar'
 
@@ -9,11 +9,8 @@ export class ToolbarPlugin implements Plugin {
   private app: Application | null = null
 
   setup(injector: Injector) {
-    this.app = viewfly({
-      context: injector,
-      root: <Toolbar/>,
-      nativeRenderer: new DomRenderer(),
-      autoUpdate: true
+    this.app = createApp(<Toolbar/>, {
+      context: injector
     })
     const viewDocument = injector.get(VIEW_DOCUMENT)
     const host = document.createElement('div')
--- src/plugins/toolbar/toolbar.tsx
+++ src/plugins/toolbar/toolbar.tsx
@@ -1,32 +1,93 @@
 import { inject, onUnmounted } from '@viewfly/core'
 import { withScopedCSS } from '@viewfly/scoped-css'
-import { Selection } from '@textbus/core'
+import { debounceTime, Selection, tap } from '@textbus/core'
+import { SelectionBridge, VIEW_DOCUMENT } from '@textbus/platform-browser'
 
 import css from './toolbar.scoped.scss'
+import { useProduce } from '@viewfly/hooks'
 
 export function Toolbar() {
   const selection = inject(Selection)
-  const subscription = selection.onChange.subscribe(() => {
-    console.log(4)
+  const viewDocument = inject(VIEW_DOCUMENT)
+  const bridge = inject(SelectionBridge)
+
+  const [viewPosition, updateViewPosition] = useProduce({
+    left: 0,
+    top: 0,
+    isHide: true,
+    opacity: 0,
+    transitionDuration: 0
+  })
+
+  const subscription = selection.onChange.pipe(
+    tap(() => {
+      if (selection.isCollapsed) {
+        updateViewPosition(draft => {
+          draft.opacity = 0
+          draft.isHide = true
+          draft.transitionDuration = 0
+        })
+      }
+    }),
+    debounceTime(300)
+  ).subscribe(() => {
+    if (selection.isCollapsed) {
+      return
+    }
+
+    const containerRect = viewDocument.getBoundingClientRect()
+    const selectionFocusRect = bridge.getRect({
+      slot: selection.focusSlot!,
+      offset: selection.focusOffset!
+    })!
+    const selectionAnchorRect = bridge.getRect({
+      slot: selection.anchorSlot!,
+      offset: selection.anchorOffset!
+    })!
+
+    const centerLeft = (selectionFocusRect.left + selectionAnchorRect.left) / 2
+
+    updateViewPosition(draft => {
+      draft.isHide = false
+      draft.transitionDuration = .15
+      draft.left = centerLeft - containerRect.left
+      draft.top = selectionFocusRect.top - containerRect.top + 10
+    })
+
+    setTimeout(() => {
+      updateViewPosition(draft => {
+        draft.opacity = 1
+        draft.top = selectionFocusRect.top - containerRect.top
+      })
+    }, 200)
   })
 
   onUnmounted(() => {
     subscription.unsubscribe()
   })
-  return withScopedCSS(css, () => (
-    <div class="editor-toolbar">
-      <div class="editor-toolbar-item">
-        <button class="editor-toolbar-btn" type="button">h1</button>
+  return withScopedCSS(css, () => {
+    const p = viewPosition()
+    return (
+      <div class="editor-toolbar" style={{
+        left: p.left + 'px',
+        top: p.top + 36 + 'px',
+        pointerEvents: p.isHide ? 'none' : 'initial',
+        opacity: p.opacity,
+        transitionDuration: p.transitionDuration + 's'
+      }}>
+        <div class="editor-toolbar-item">
+          <button class="editor-toolbar-btn" type="button">h1</button>
+        </div>
+        <div class="editor-toolbar-item">
+          <button class="editor-toolbar-btn" type="button">h2</button>
+        </div>
+        <div class="editor-toolbar-item">
+          <button class="editor-toolbar-btn" type="button">h3</button>
+        </div>
+        <div class="editor-toolbar-item">
+          <button class="editor-toolbar-btn" type="button">h4</button>
+        </div>
       </div>
-      <div class="editor-toolbar-item">
-        <button class="editor-toolbar-btn" type="button">h2</button>
-      </div>
-      <div class="editor-toolbar-item">
-        <button class="editor-toolbar-btn" type="button">h3</button>
-      </div>
-      <div class="editor-toolbar-item">
-        <button class="editor-toolbar-btn" type="button">h4</button>
-      </div>
-    </div>
-  ))
+    )
+  })
 }
