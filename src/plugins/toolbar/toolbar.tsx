5e6ee37ec57840a6f8600394e24882902f88fd7c
tbhuabi <464328895@qq.com>
2023-08-22 00:21:23 +0800

    feat: 优化行内工具条交互

--- src/plugins/toolbar/toolbar.tsx
+++ src/plugins/toolbar/toolbar.tsx
@@ -1,6 +1,6 @@
 import { inject, onUnmounted } from '@viewfly/core'
 import { withScopedCSS } from '@viewfly/scoped-css'
-import { debounceTime, Selection, tap } from '@textbus/core'
+import { delay, filter, fromEvent, map, Selection, Subscription } from '@textbus/core'
 import { SelectionBridge, VIEW_DOCUMENT } from '@textbus/platform-browser'
 
 import css from './toolbar.scoped.scss'
@@ -19,51 +19,55 @@ export function Toolbar() {
     transitionDuration: 0
   })
 
-  const subscription = selection.onChange.pipe(
-    tap(() => {
-      if (selection.isCollapsed) {
+  let mouseupSubscription = new Subscription()
+
+  function bindMouseup() {
+    mouseupSubscription = fromEvent(viewDocument, 'mouseup').pipe(
+      delay(100),
+      filter(() => {
+        return !selection.isCollapsed
+      }),
+      map(() => {
+        const containerRect = viewDocument.getBoundingClientRect()
+        const selectionFocusRect = bridge.getRect({
+          slot: selection.focusSlot!,
+          offset: selection.focusOffset!
+        })!
+
+        const centerLeft = selectionFocusRect.left // (selectionFocusRect.left + selectionAnchorRect.left) / 2
+        const focusEnd = selection.focusSlot === selection.endSlot && selection.focusOffset === selection.endOffset
+        const top = focusEnd ? selectionFocusRect.top - containerRect.top : selectionFocusRect.top - containerRect.top - 80
+
         updateViewPosition(draft => {
-          draft.opacity = 0
-          draft.isHide = true
-          draft.transitionDuration = 0
+          draft.isHide = false
+          draft.transitionDuration = .15
+          draft.left = centerLeft - containerRect.left
+          draft.top = top + 10
         })
-      }
-    }),
-    debounceTime(300)
-  ).subscribe(() => {
-    if (selection.isCollapsed) {
-      return
-    }
-
-    const containerRect = viewDocument.getBoundingClientRect()
-    const selectionFocusRect = bridge.getRect({
-      slot: selection.focusSlot!,
-      offset: selection.focusOffset!
-    })!
-    const selectionAnchorRect = bridge.getRect({
-      slot: selection.anchorSlot!,
-      offset: selection.anchorOffset!
-    })!
-
-    const centerLeft = (selectionFocusRect.left + selectionAnchorRect.left) / 2
-
-    updateViewPosition(draft => {
-      draft.isHide = false
-      draft.transitionDuration = .15
-      draft.left = centerLeft - containerRect.left
-      draft.top = selectionFocusRect.top - containerRect.top + 10
-    })
-
-    setTimeout(() => {
+        return top
+      }),
+      delay(200)
+    ).subscribe((top) => {
       updateViewPosition(draft => {
         draft.opacity = 1
-        draft.top = selectionFocusRect.top - containerRect.top
+        draft.top = top
       })
-    }, 200)
-  })
+    })
+  }
 
+
+  const mousedownSubscription = fromEvent(document, 'mousedown').subscribe(() => {
+    mouseupSubscription.unsubscribe()
+    updateViewPosition(draft => {
+      draft.opacity = 0
+      draft.isHide = true
+      draft.transitionDuration = 0
+    })
+    bindMouseup()
+  })
   onUnmounted(() => {
-    subscription.unsubscribe()
+    mousedownSubscription.unsubscribe()
+    mouseupSubscription.unsubscribe()
   })
   return withScopedCSS(css, () => {
     const p = viewPosition()
