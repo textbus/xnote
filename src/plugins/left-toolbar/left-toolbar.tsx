b974e7ecb9b2599c9f9531ad0b5b01ebe05b04a6
tbhuabi <464328895@qq.com>
2024-05-05 23:19:48 +0800

    feat: 优化左侧工具条状态同步

--- src/plugins/left-toolbar/left-toolbar.tsx
+++ src/plugins/left-toolbar/left-toolbar.tsx
@@ -26,15 +26,11 @@ import { DomAdapter } from '@textbus/platform-browser'
 
 import css from './left-toolbar.scoped.scss'
 import { RefreshService } from '../../services/refresh.service'
-import { headingAttr } from '../../textbus/attributes/heading.attr'
 import { MenuItem } from '../../components/menu-item/menu-item'
 import { useActiveBlock } from '../hooks/active-block'
 import { Divider } from '../../components/divider/divider'
 import { useBlockTransform } from '../hooks/block-transform'
-import { ParagraphComponent } from '../../textbus/components/paragraph/paragraph.component'
 import { SourceCodeComponent } from '../../textbus/components/source-code/source-code.component'
-import { BlockquoteComponent } from '../../textbus/components/blockqoute/blockquote.component'
-import { TodolistComponent } from '../../textbus/components/todolist/todolist.component'
 import { RootComponent } from '../../textbus/components/root/root.component'
 import { Dropdown } from '../../components/dropdown/dropdown'
 import { TableComponent } from '../../textbus/components/table/table.component'
@@ -56,6 +52,7 @@ export function LeftToolbar() {
     const active = activeSlot()
     if (active) {
       selection.setPosition(active, active.length)
+      selection.restore()
       toBlock(v)
       activeSlot.set(selection.focusSlot)
       refreshService.onRefresh.next()
@@ -100,6 +97,7 @@ export function LeftToolbar() {
     ).subscribe(slot => {
       activeSlot.set(slot)
       if (slot) {
+        checkStates(slot)
         const nativeNode = adapter.getNativeNodeByComponent(slot.parent!)!
         updatePosition(draft => {
           const containerRect = docContentContainer.getBoundingClientRect()
@@ -120,6 +118,9 @@ export function LeftToolbar() {
   const subscription = merge(textbus.onChange, selection.onChange).pipe(
     debounceTime(20)
   ).subscribe(() => {
+    if (activeSlot()) {
+      return
+    }
     refreshService.onRefresh.next()
   }).add(
     selection.onChange.pipe(throttleTime(30)).subscribe(() => {
@@ -193,25 +194,24 @@ export function LeftToolbar() {
     const position = positionSignal()
     const slot = activeSlot()
     let activeNode = <span class="xnote-icon-pilcrow"/>
+    const states = checkStates()
+
     if (slot) {
-      const component = slot.parent!
       const types: [boolean, JSXNode][] = [
-        [component.name === ParagraphComponent.componentName, <span class="xnote-icon-pilcrow"/>],
-        [component.name === SourceCodeComponent.componentName, <span class="xnote-icon-source-code"/>],
-        [component.name === BlockquoteComponent.componentName, <span class="xnote-icon-quotes-right"/>],
-        [component.name === TodolistComponent.componentName, <span class="xnote-icon-checkbox-checked"/>],
+        [states.paragraph, <span class="xnote-icon-pilcrow"/>],
+        [states.sourceCode, <span class="xnote-icon-source-code"/>],
+        [states.blockquote, <span class="xnote-icon-quotes-right"/>],
+        [states.todolist, <span class="xnote-icon-checkbox-checked"/>],
+        [states.table, <span class="xnote-icon-table"/>],
+        [states.unorderedList, <span class="xnote-icon-list"/>],
+        [states.orderedList, <span class="xnote-icon-list-numbered"/>],
+        [states.h1, <span class="xnote-icon-heading-h1"/>],
+        [states.h2, <span class="xnote-icon-heading-h2"/>],
+        [states.h3, <span class="xnote-icon-heading-h3"/>],
+        [states.h4, <span class="xnote-icon-heading-h4"/>],
+        [states.h5, <span class="xnote-icon-heading-h5"/>],
+        [states.h6, <span class="xnote-icon-heading-h6"/>],
       ]
-      const heading = slot.getAttribute(headingAttr)
-      if (heading) {
-        types.unshift(
-          ['h1' === heading, <span class="xnote-icon-heading-h1"/>],
-          ['h2' === heading, <span class="xnote-icon-heading-h2"/>],
-          ['h3' === heading, <span class="xnote-icon-heading-h3"/>],
-          ['h4' === heading, <span class="xnote-icon-heading-h4"/>],
-          ['h5' === heading, <span class="xnote-icon-heading-h5"/>],
-          ['h6' === heading, <span class="xnote-icon-heading-h6"/>],
-        )
-      }
 
       for (const t of types) {
         if (t[0]) {
@@ -220,8 +220,6 @@ export function LeftToolbar() {
         }
       }
     }
-
-    const states = checkStates(activeSlot())
 
     return (
       <div class="left-toolbar" ref={toolbarRef}>
