2c34fc0e1a1321a3e6e2c3641b5dcb8e798f690e
faxinwang <1094828998@qq.com>
2024-05-25 23:26:39 +0800

    feat: fix list Tab shortcut

--- src/textbus/components/list/list.component.tsx
+++ src/textbus/components/list/list.component.tsx
@@ -156,7 +156,7 @@ export class ListComponent extends Component<ListComponentState> {
         key: 'Tab'
       },
       action: (): boolean | void => {
-        updateAfterList(this)
+        Promise.resolve().then(() => updateAfterList(this))
         return false
       }
     })
