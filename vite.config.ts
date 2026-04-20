4c0f34c8a8ac358dd5b2e5e85fa6b011f83653be
tbhuabi <464328895@qq.com>
2026-04-20 23:15:49 +0800

    chore: 开发环境变更为 vite

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import path from 'path'
+import ip from 'ip'
+import { defineConfig } from 'vite'
+import swc from 'vite-plugin-swc-transform'
+import checker from 'vite-plugin-checker'
+import viteScopedCssPlugin from '@viewfly/devtools/vite-scoped-css-plugin'
+
+export default defineConfig({
+  plugins: [
+    viteScopedCssPlugin(true),
+    swc({
+      swcOptions: {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            decorators: true,
+            tsx: true,
+          },
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+            useDefineForClassFields: false,
+            react: {
+              runtime: 'automatic',
+              importSource: '@viewfly/core',
+              throwIfNamespace: true,
+            },
+          },
+        },
+      }
+    }),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}" "./index.tsx"',
+      },
+    })
+  ],
+  resolve: {
+    alias: {
+      '@textbus/xnote': path.resolve(__dirname, './src/public-api.ts'),
+    }
+  },
+  server: {
+    host: ip.address(),
+    port: 5636,
+    open: true,
+    proxy: {
+      '/api/llm': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (p) => p.replace(/^\/api\/llm/, '/llm')
+      }
+    }
+  }
+})
